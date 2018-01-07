eac420a3b88e73e66e7fe02ded7bcf3f6f122df4
PNKMR1221 <PNKMR1221@gmail.com>
2018-01-07 16:44:02 +0000

    added day3 scripts

---
+++ Day3/commandlinearguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hi how are you "
+echo "my script name is $0"
+echo "The first command line argument is: $1"
+echo "The second command line argumet is: $2"
+echo "The no of arguments are: $#"
+echo "The all arguments are : $*"
---
+++ Day3/start_stop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/webserver/bin/httpd -k $1
